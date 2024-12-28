014efa57ad22da7b4e8af06d645610538f5cd532
mcmichaelau2 <austin@praxispro.ai>
2024-12-28 12:09:31 -0500

    log

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3237,7 +3237,7 @@ class GenerationMixin:
             `return_dict_in_generate=True` or a [`~generation.GenerateEncoderDecoderOutput`] if
             `model.config.is_encoder_decoder=True`.
         """
-        print("------------- _sample -------------")
+        print("------------- _sample :) -------------")
         # init values
         pad_token_id = generation_config._pad_token_tensor
         output_attentions = generation_config.output_attentions
