d18e8ef5ccb1c8b0a14c3237d9d3d32d7883fe88
mcmichaelau2 <austin@praxispro.ai>
2025-01-07 11:49:08 -0500

    logs

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3324,7 +3324,8 @@ class GenerationMixin:
                 next_tokens = torch.argmax(layer_scores, dim=-1)
                 all_layers_next_tokens[layer_idx].append(next_tokens)
 
-            print(f"All layers next tokens: {all_layers_next_tokens}")
+            print(f"First layer next tokens: {all_layers_next_tokens[0]}")
+            print(f'Last layer next tokens: {all_layers_next_tokens[-1]}')
 
 
 
